ec94f66a193e6e3f8a9043359a48d6b5431e5ae8
praka <praka@PRAKASHLAPTOP>
2024-12-04 11:41:15 +0530

    Add project files.

---
+++ LooseCouplingDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LooseCouplingDemo
+{
+   public interface IEngine
+    {
+        void StartEngine();
+    }
+
+    public class DieselEngine : IEngine
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Starting Diesel Engine...");
+        }
+    }
+
+    public class PetrolEngine : IEngine
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Starting Petrol Engine...");
+        }
+    }
+    public class ElectricEngine : IEngine
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Starting Electric Engine...");
+        }
+    }
+
+    public class Car  
+    {
+        private IEngine _engine;
+        public Car(IEngine engine)
+        {
+            _engine = engine;
+        }
+
+        public void StartCar()
+        {
+            _engine.StartEngine();
+            Console.WriteLine("Car started...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Loose coupling Demo
+            DieselEngine engine1 = new DieselEngine();
+            Car myCar = new Car(engine1);  // we are passing Diesel Engine object to car class 
+            myCar.StartCar();
+            //Passing petrol engine object to car class
+            PetrolEngine engine2 = new PetrolEngine();
+            Car myCar2 = new Car(engine2);
+            myCar2.StartCar();
+
+
+            Console.ReadKey();
+        }
+    }
+}
